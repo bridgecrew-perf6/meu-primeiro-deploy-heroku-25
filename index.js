5a6d33f2595c10aebe0dce77c85a517e15f70ea5
Ingo Reichert Junior <ingoreichertjr@gmail.com>
2022-03-17 11:27:39 -0300

    Fix process

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config();
 
 const app = express();
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => res.send(`<h1> Executando na porta ${PORT} </h1>`));
